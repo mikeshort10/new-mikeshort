0ae5191c3475fa85d95049fa22ab8f4ec1a02c90
Mike Short <mikeshort2014@gmail.com>
2020-05-27 14:15:27 -0400

    restyle homepage

--- components/Navbar.tsx
+++ components/Navbar.tsx
@@ -3,7 +3,7 @@ import Link from "next/link";
 
 export const Navbar: React.FC = () => {
   return (
-    <nav className="bg-gray-800 h-16 text-gray-200 fixed top-0 left-0 flex justify-between w-full p-5 z-50 border-b-2 border-gray-200">
+    <nav className="bg-blue-800 h-16 text-gray-200 fixed top-0 left-0 flex justify-between w-full p-5 z-50 border-b-2 border-gray-200">
       <Link href="/">
         <div className="flex justify-center items-center cursor-pointer">
           <img src="/favicon.ico" alt="logo" className="h-10 rounded" />
--- pages/index.tsx
+++ pages/index.tsx
@@ -4,8 +4,10 @@ import { Layout } from "../components/Layout";
 import axios from "axios";
 
 const P: React.FC = ({ children }) => (
-  <p className="py-2 sm:px-3 max-w-5xl mx-auto">{children}</p>
+  <p className="py-2 my-2 mx-auto text-gray-400">{children}</p>
 );
+
+const H3: React.FC = ({ children }) => <h3 className="text-2xl">{children}</h3>;
 
 const Home: NextPage = () => {
   if (typeof window !== "undefined") {
@@ -23,43 +25,34 @@ const Home: NextPage = () => {
   return (
     <Layout title="Mike Short">
       <div className="home-pic h-screen bg-cover bg-center bg-no-repeat relative">
-        <h1 className="text-center text-6xl pt-32 absolute z-10 w-full text-gray-200 tracking-widest font-mono">
+        <h1 className="text-6xl pt-24 absolute z-10 text-blue-800 tracking-widest font-mono md:pr-20 lg:pr-24 right-0">
           Mike Short
         </h1>
-        <div className="h-full bg-gray-800 opacity-50"></div>
+        <div className="h-full bg-gray-200 opacity-25"></div>
       </div>
       <div
         id="about"
-        className="px-10 font-mono bg-gray-800 text-white pt-10 pb-16"
+        className="px-10 font-mono bg-blue-800 text-white pt-10 pb-16"
       >
-        <h2 className="text-4xl text-center">About Me</h2>
-        <P>
-          I started coding as professional development. I worked with
-          international students and found myself needing resources for them
-          that weren&apos;t readily available or were outside the budget of our
-          program. However, about a month into the FreeCodeCamp curriculum, I
-          found myself working until 2-3am every night to develop projects. I
-          had to turn to myself and ask, &ldquo;Why am I treating this like a
-          side gig?&rdquo;
-        </P>
-        <P>
-          Since then I&apos;ve worked as a freelancer, helping others make their
-          dreams and ideas a reality. I&apos;ve also been certified as a Full
-          Stack Developer and have gone from making stop-gap products to fully
-          functioning applications. What has not changed, however, is that I
-          still design my sites for English language learners. What this means
-          is that I rely less on written instruction than on the flow and
-          simplicity of the user experience.
-        </P>
-        <P>
-          Both in and outside of my coding career, I have frequently been
-          described by those I work with as hard-working and innovative. As one
-          former employer said &ldquo;Mike will never come to you with a problem
-          that he&apos;s discovered, without having at least one viable solution
-          for fixing it&rdquo;. This is the effort that I will bring to your
-          project. Have a deadline? I&apos;ll meet it. Having issues? I&apos;ll
-          help solve them.
-        </P>
+        <div className="max-w-3xl mx-auto">
+          <h2 className="text-4xl text-center">About Me</h2>
+          <H3>Mike the Programmer</H3>
+          <P>
+            Full-stack developer and big believer in functional, beautiful code.
+            Favorite languages include TypeScript and GraphQL.
+          </P>
+          <H3>Mike the Professional</H3>
+          <P>
+            Hard-working, innovative developer who loves learning new tech and
+            providing create solutions to everyday problems.
+          </P>
+          <H3>Mike the Person</H3>
+          <P>
+            {" "}
+            Outdoorsman, lover of literature, and always down for a craft beer
+            from a local microbrewery.
+          </P>
+        </div>
       </div>
     </Layout>
   );
